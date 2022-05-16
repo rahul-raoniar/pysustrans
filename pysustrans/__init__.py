3f60653354239619e9526dd3caf153044fa3adf8
Rahul Raoniar <rahul.raoniar@outlook.com>
2022-05-16 15:13:51 +0530

    Added the module and updated the doc string

--- pysustrans/__init__.py
+++ pysustrans/__init__.py
@@ -1,4 +1,12 @@
-from .association import Association
+"""
+pysustrans
+========== 
+A subpackage for estimating association and correlation
+among variables
+"""
+
+from pysustrans.association import Association
+from pysustrans.correlation import Correlation
 
 # Current version
 __version__ = "0.0.110"
